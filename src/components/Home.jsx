a653e6149f2790dff98e1ed1c0efbc75986181eb
Leonel Ramirez <leonel_ramirez93@hotmail.com.ar>
2022-04-30 22:30:27 -0300

    changing buttom

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+
 
 const Home = () => {
   return (
@@ -11,13 +11,7 @@ const Home = () => {
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Front-end Developer</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a  junior front-end developer. Currently, I'm focused on
           building responsive web applications.</p>
-        <div>
-            <button className='text-white group border-2 px-6 p-y3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'> View work
-            <span className='group-hover:rotate-90 duration-300'>
-            <HiArrowNarrowRight className='ml-3 '/>
-            </span>
-            </button>
-        </div>
+      
       </div>
     </div>
   )
