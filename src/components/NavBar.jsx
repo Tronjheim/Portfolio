9b49f6bbe0307a04d157b326eebfef917bb11ae6
Leonel Ramirez <leonel_ramirez93@hotmail.com.ar>
2023-04-15 18:54:01 -0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,14 @@
-import React, {useState} from 'react'
-import {FaBars, FaTimes,FaGithub,FaLinkedin,} from 'react-icons/fa'
+import React, { useState } from 'react'
+import { FaBars, FaTimes, FaGithub, FaLinkedin, } from 'react-icons/fa'
 import Logo from '../assets/logo.png'
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 
-const NavBar =() => {
-  const[nav,setNav]= useState(false)
-  const handleClick= ()=> setNav(!nav)
+const NavBar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
   return (
     <div
-    className='fixed
+      className='fixed
     w-full
     h-[80px]
     flex
@@ -19,73 +19,73 @@ const NavBar =() => {
     text-gray-300
     '>
       <div>
-        <img src={Logo} alt="Logo Image" style={{width:'100px'}}/>
+        <img src={Logo} alt="Logo Image" style={{ width: '100px' }} />
       </div>
 
-        {/*menu*/}
-      
-          <ul className='hidden md:flex'> 
-              <li>
-              <Link to="home"smooth={true} duration={500}>Home</Link>
-              </li>
-              <li>
-                <Link to="about"smooth={true} duration={500}>About</Link>
-                </li>
-              <li>
-                <Link to="skills"smooth={true} duration={500}>Skills</Link>
-                </li>
-              <li>
-                <Link to="work"smooth={true} duration={500}>Work</Link>
-                </li>
-              <li>
-                <Link to="contact"smooth={true} duration={500}>Contact</Link>
-                </li>
-          </ul>
-      
+      {/*menu*/}
 
-        {/*Hamburger*/}
-            <div onClick={handleClick} className='md:hidden z-10'>
-                {!nav ? <FaBars size={50}/> : <FaTimes/>}
-            </div>
+      <ul className='hidden md:flex'>
+        <li>
+          <Link to="home" smooth={true} duration={500}>Home</Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>About</Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>Skills</Link>
+        </li>
+        <li>
+          <Link to="work" smooth={true} duration={500}>Work</Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>Contact</Link>
+        </li>
+      </ul>
 
-        {/*Mobile menu*/}
-            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-              <li className='py-6 text-4xl'>
-              <Link onClick={handleClick} to="home"smooth={true} duration={500}>Home</Link>
-              </li>
-              <li className='py-6 text-4xl'>
-                {' '}
-                <Link onClick={handleClick} to="about"smooth={true} duration={500}>About</Link>
-              </li>
-              <li className='py-6 text-4xl'>
-              {' '}
-                <Link onClick={handleClick} to="skills"skills={true} duration={500}>Skills</Link>
-              </li>
-              <li className='py-6 text-4xl'>
-              {' '}
-                <Link onClick={handleClick} to="work"smooth={true} duration={500}>Work</Link>
-              </li>
-              <li className='py-6 text-4xl'>
-              {' '}
-                <Link onClick={handleClick} to="contact"smooth={true} duration={500}>Contact</Link>
-              </li>
-            </ul>
 
-        {/*Social icons*/}
-            <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-              <ul>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                  <a className='flex justify-between items-center w-full text-gray-300' href="http://www.linkedin.com/in/leonel-ramirez-93446b100" target="_blank">
-                    Linkedin <FaLinkedin size={30}/>
-                  </a>
-                </li>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                  <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/Tronjheim?tab=repositories" target="_blank">
-                    Github <FaGithub size={30}/>
-                  </a>
-                </li>
-              </ul>
-            </div>
+      {/*Hamburger*/}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars size={50} /> : <FaTimes />}
+      </div>
+
+      {/*Mobile menu*/}
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>Home</Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>About</Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to="skills" skills={true} duration={500}>Skills</Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>Work</Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>Contact</Link>
+        </li>
+      </ul>
+
+      {/*Social icons*/}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/leonel-ramirez-dev/" target="_blank">
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/Tronjheim?tab=repositories" target="_blank">
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
